d4038459005e3b232dc53c7aeaf0eadee30fec7c
Shahmir <75688231+mind999hacker@users.noreply.github.com>
2024-03-19 02:04:06 -0700

    Update Machine.h
    comments

--- Machine.h
+++ Machine.h
@@ -4,9 +4,19 @@ class Machine
 {
     public:
         Machine();
+        //main loop to active different functions of the code by changing the active state 
         void loop();
+
+        ////goes through each antenna and gets all the tags in a room 
+        //and puts it in a dynamic linked list
         void add();
+
+        //inventary tracking constantly updates location of all tags and displays them at the end 
+        //STILL NEEDS WORK NOT READY
         void track();
+
+        //returns array of 1 and 0 depending on which antenna picked up the tag 
+        //given to it in the argument
         void fetch(unsigned int tagToFind, int *anttenaArray);
     private:
         State activeState;
